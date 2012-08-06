16800cd6d0e60014a03fb0c8fcbbb2463f9d02fd
Denis Krienbühl <denis.krienbuehl@gmail.com>
2012-08-06 12:08:16 +0200

    Links to shared setuphandler functions

--- seantis/dir/contacts/setuphandlers.py
+++ seantis/dir/contacts/setuphandlers.py
@@ -1,26 +1,4 @@
-from zope.component.hooks import getSite
-
-def get_fti(typename):
-    types = getSite().portal_types
-
-    if typename in types:
-        return types[typename]
-    else:
-        return None
-
-def add_behavior(fti, behavior):
-    if fti:
-        behaviors = list(fti.behaviors)
-        if not behavior in behaviors:
-            behaviors.append(behavior)
-            fti.behaviors = tuple(behaviors)
-
-def remove_behavior(fti, behavior):
-    if fti:
-        behaviors = list(fti.behaviors)
-        if behavior in behaviors:
-            behaviors.remove(behavior)
-            fti.behaviors = tuple(behaviors)
+from seantis.dir.base.setuphandlers import get_fti, add_behavior, remove_behavior
 
 def uninstallOldBehaviors(context, logger=None):
     
